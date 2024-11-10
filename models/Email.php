cc38f81beac13d0c35f86b687ae3bde53b14e12a
Efrain <carlosefrainchallo1@gmail.com>
2024-11-09 19:39:18 -0500

    Se agrega el Email.php y el include

---
+++ models/Email.php
@@ -0,0 +1,42 @@
+<?php
+require '../include/vendor/autoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require_once("../config/conexion.php");
+require_once("../models/Usuario.php");
+/**Mesa de Partes */
+class Email extends PHPMailer{
+    protected $gCorreo='carlosefrain777@limpiobe.net';
+    protected $gContrasena='Mesadepartes@7';
+
+    public function registrar($usu_id){
+        $this->isSMTP();
+        $this->Port=587;
+        $this->SMTPAuth=true;
+        $this->SMTPSecure='tls';
+        
+        $this->Username=$this->gCorreo;
+        $this->Password=$this->gContrasena;
+        $this->setFrom($this->gCorreo,"Registro en mesa de partes Efrain");
+
+        $this->CharSet='UTF8';
+        $this->addAddress("Carloschallo1@hotmail.com");
+        $this->isHTML(true);
+        $this->Subject='Registro en Mesa de Partes';
+
+        $cuerpo=file_get_contents("../assets/email/registrar.html");
+        $cuerpo=str_replace("xusuariocorreo",$$usu_id,$cuerpo);
+
+        $this->Body=$cuerpo;
+        $this->AltBody=strip_tags("Confirmar Registro");
+
+        try {
+            $this->send();
+            return true;
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+}
+?>
