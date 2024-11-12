14edcc00537201812740e9137574d093cb5adec5
Efrain <carlosefrainchallo1@gmail.com>
2024-11-11 21:14:14 -0500

    Funciona enviar el correo; pero el sweet alert no funciona

--- controller/usuario.php
+++ controller/usuario.php
@@ -3,12 +3,16 @@
 //TODO: Incluye el archivo
  require_once("../config/conexion.php");
  require_once("../models/Usuario.php");
+ require_once("../models/Email.php");
  $usuario=new Usuario();
+ $email=new Email();
  switch ($_GET["op"]) {
     case 'registrar':
         $datos=$usuario->get_usuario_correo($_POST["usu_correo"]);
         if (is_array($datos)==true and count($datos)==0) {
             $usuario->registrar_usuario($_POST["usu_nomape"],$_POST["usu_correo"],$_POST["usu_pass"]);
+            //Email
+            $email->registrar(1); //Reemplazar con el ID del usuario registrado
             //Identificado
             echo "1";
         }else{
