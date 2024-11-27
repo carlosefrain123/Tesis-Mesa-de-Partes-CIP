88591b2d85f231a73ed23450cbdcb9a08c5dfe25
Efrain <carlosefrainchallo1@gmail.com>
2024-11-27 00:23:42 -0500

    Se visualiza en el confirmar.js

---
+++ view/confirmar/confirmar.js
@@ -0,0 +1,9 @@
+/* console.log("Hola"); */
+$(document).ready(function(){
+    const url=window.location.href;
+    const params=new URLSearchParams(new URL(url).search);
+    const confirmar=params.get("id");
+    const decoded_id=decodeURIComponent(confirmar);
+    const id=decoded_id.replace(/\s/g,'+');
+    console.log(id);
+})
