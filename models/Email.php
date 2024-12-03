cf86c720af84cef28c925b7ef02cfc028d504389
Efrain <carlosefrainchallo1@gmail.com>
2024-12-03 06:36:52 -0500

    Se modifica el Email

--- models/Email.php
+++ models/Email.php
@@ -51,5 +51,44 @@ class Email extends PHPMailer{
             return false;
         }
     }
+    public function recuperar($usu_correo){
+        $conexion=new Conectar();
+        $usuario=new Usuario();
+        $datos=$usuario->get_usuario_correo($usu_correo);
+
+        /* $iv=openssl_random_pseudo_bytes(openssl_cipher_iv_length($this->cipher));
+        $cifrado=openssl_encrypt($user_id,$this->cipher,$this->key,OPENSSL_RAW_DATA,$iv);
+        $textoCifrado=base64_encode($iv.$cifrado); */
+
+        $this->IsSMTP();
+        $this->Host='smtp.hostinger.com';
+        $this->Port=465;
+        $this->SMTPAuth=true;
+        $this->SMTPSecure='ssl';
+        
+        $this->Username=$this->gCorreo;
+        $this->Password=$this->gContrasena;
+        $this->setFrom($this->gCorreo,"Recuperar Contraseña mesa de partes Efrain");
+
+        $this->CharSet='UTF8';
+        /* $this->addAddress($usu_correo); */
+        $this->addAddress($datos[0]["usu_correo"]);
+        $this->IsHTML(true);
+        $this->Subject="Registro en mesa de partes Efrain";
+
+        $cuerpo=file_get_contents("../assets/email/registrar.html");
+        $url=$conexion->ruta().'view/confirmar/?id=';
+        $cuerpo=str_replace('xlinkcorreourl',$url,$cuerpo);
+
+        $this->Body=$cuerpo;
+        $this->AltBody=strip_tags("Recuperar Contraseña");
+
+        try {
+            $this->send();
+            return true;
+        } catch (Exception $e) {
+            return false;
+        }
+    }
 }
 ?>
