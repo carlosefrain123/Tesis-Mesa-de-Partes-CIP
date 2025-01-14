16bc64c6ebdf5487a42c504399ff2a4c679fbdc8
Efrain <carlosefrainchallo1@gmail.com>
2025-01-14 00:55:08 -0500

    Se ordena y se divide la estructura del sistema. El header, el footer, menu y sidebar. El logout se crea, para que cierre sesión, si funciona correctamente.

---
+++ view/html/footer.php
@@ -0,0 +1,17 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                <p class="mb-0">© <script>
+                        document.write(new Date().getFullYear())
+                    </script> Todos los derechos reservados: <b>CIP CDL</b></p>
+            </div>
+            <div class="col-sm-6">
+                <div class="text-sm-end d-none d-sm-block">
+                    <!--                     Design & Develop by <a href="#!" class="text-decoration-underline">Themesbrand</a>
+ -->
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
