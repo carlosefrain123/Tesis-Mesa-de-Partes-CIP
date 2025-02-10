d996b8bc73824b1dd28c40fa27a7f7d6edcfe488
Efrain <carlosefrainchallo1@gmail.com>
2025-02-09 20:02:55 -0500

    Al momento de cerrar sesión dentro del sistema de colaborador, te manda a la interfaz de colaborador

--- view/html/logout.php
+++ view/html/logout.php
@@ -1,6 +1,11 @@
 <?php
     require_once("../../config/conexion.php");
     session_destroy();
-    header("Location:".Conectar::ruta()."index.php");
+    if ($_SESSION["rol_id"]==1) {
+        header("Location:".Conectar::ruta()."index.php");
+    } elseif($_SESSION["rol_id"]==2||$_SESSION["rol_id"]==3) {
+        header("Location:".Conectar::ruta()."view/accesopersonal/index.php");
+    }
+    
     exit();
 ?>
