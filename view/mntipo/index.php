57f9e758064ce8553a02a724c12f7f5be6160ed1
Efrain <carlosefrainchallo1@gmail.com>
2025-02-12 00:10:00 -0500

    Se agrega las interfaces de buscartramite,gestionartramite,mntarea,mnttipo,mnttramite,mntusuario

--- view/mntipo/index.php
+++ view/mntipo/index.php
@@ -7,7 +7,7 @@
 <html lang="es">
 
 <head>
-    <title>CIP | Inicio Mesa de Partes Colaborador</title>
+    <title>CIP | Mnt Trámite</title>
     <?php require_once("../html/head.php") ?>
 </head>
 
@@ -34,7 +34,7 @@
                     <div class="row">
                         <div class="col-12">
                             <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                                <h4 class="mb-sm-0 font-size-18">Inicio Colaborador</h4>
+                                <h4 class="mb-sm-0 font-size-18">Mantenimiento de Trámite</h4>
 
                                 <div class="page-title-right">
                                     <ol class="breadcrumb m-0">
