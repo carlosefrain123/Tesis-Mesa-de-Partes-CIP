1b697fd45c31c04f46f15597bba4b792b25c5448
Efrain <carlosefrainchallo1@gmail.com>
2025-02-22 21:50:55 -0500

    Se cambia el nombre y el logo del CIP

--- view/html/header.php
+++ view/html/header.php
@@ -5,19 +5,19 @@
             <div class="navbar-brand-box">
                 <a href="index.html" class="logo logo-dark">
                     <span class="logo-sm">
-                        <img src="../../assets/picture/logo-sm.svg" alt="" height="24">
+                        <img src="../../assets/picture/LogoCIP.png" alt="" height="24">
                     </span>
                     <span class="logo-lg">
-                        <img src="../../assets/picture/logo-sm.svg" alt="" height="24"> <span class="logo-txt">Minia</span>
+                        <img src="../../assets/picture/LogoCIP.png" alt="" height="24"> <span class="logo-txt">CIP - Lambayeque</span>
                     </span>
                 </a>
 
                 <a href="index.html" class="logo logo-light">
                     <span class="logo-sm">
-                        <img src="../../assets/picture/logo-sm.svg" alt="" height="24">
+                        <img src="../../assets/picture/LogoCIP.png" alt="" height="24">
                     </span>
                     <span class="logo-lg">
-                        <img src="../../assets/picture/logo-sm.svg" alt="" height="24"> <span class="logo-txt">Minia</span>
+                        <img src="../../assets/picture/LogoCIP.png" alt="" height="24"> <span class="logo-txt">CIP - Lambayeque</span>
                     </span>
                 </a>
             </div>
